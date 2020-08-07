d279f65e50db998d2afd1c1f3a12e167105b6e07
renankanu <renankanu@gmail.com>
2020-08-07 13:32:58 -0300

    Refactor root Layout Element

--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -1,10 +1,10 @@
 import styled from 'styled-components'
 
 const LayoutElement = styled.div`
-  width: 100%;
   max-width: 1024px;
   margin: 0 auto;
   min-height: 100vh;
+  flex-direction: column;
   display: grid;
   grid-template-rows: auto 1fr auto;
 `
