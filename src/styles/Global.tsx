3e181e15147d10de95fffe8fcbb193a7da7f2376
renankanu <renankanu@gmail.com>
2020-08-11 13:31:33 -0300

    change background ok

--- src/styles/Global.tsx
+++ src/styles/Global.tsx
@@ -20,7 +20,7 @@ html {
 body {
   width: 100%;
   height: 100%;
-  background: url("/background.jpg") no-repeat center fixed;
+  background: url("/backg.jpg") no-repeat center fixed;
   background-size: cover;
   color: ${({ theme }) => theme.white};
   overflow-x: hidden;
