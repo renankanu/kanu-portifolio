58546bfc214e093e2daf508174443a5bdd698e44
renankanu <renankanu@gmail.com>
2020-08-11 13:24:04 -0300

    change background teste

--- src/styles/Global.tsx
+++ src/styles/Global.tsx
@@ -20,7 +20,8 @@ html {
 body {
   width: 100%;
   height: 100%;
-  background: ${({ theme }) => theme.backgroundApp};
+  background: url("/background.jpg") no-repeat center fixed;
+  background-size: cover;
   color: ${({ theme }) => theme.white};
   overflow-x: hidden;
 }
