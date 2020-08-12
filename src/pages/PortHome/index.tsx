f8437b28645cec13a7da239770b9f991dec2c418
renankanu <renankanu@gmail.com>
2020-08-11 23:41:20 -0300

    BugFix in create build

--- src/pages/PortHome/index.tsx
+++ src/pages/PortHome/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import About from 'src/components/About'
-import { Container } from './styles'
+import Container from './styles'
 import HeaderLanding from 'src/components/HeaderLanding'
 import WorkExperience from 'src/components/WorkExperience'
 import Skill from 'src/components/Skill'
